f6b3e9f0b4861d21898902a9133ae60483e52d55
Cristian Javier Alvarez <cjavi26000@hotmail.com>
2019-06-27 13:41:26 -0600

    Primer Commit del juego
    Commit por Cristian

---
+++ Princesa De los Arcos/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 4f;
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mov = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + mov, speed * Time.deltaTime);
+    }
+}
